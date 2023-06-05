5f624e0ed6f7c058055186d88822028418c502a4
Cgbrl28 <cristiano1996@hotmail.com>
2023-06-05 11:48:42 -0300

    Update: Incorporating version v3.2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace INF099
             Console.WriteLine("Adiciando tag v2.0"); 
             Console.WriteLine("Adicionando tag v3.0");
             Console.WriteLine("Adicionando tag v3.1");
+            Console.WriteLine("Adicionando tag v3.2");
         }
     }
 }
